39106858dd4e95dbf76a886d19ecb1c6df531429
Actat <akio.toyosh@gmail.com>
2022-06-22 09:22:16 +0900

    開始コドンをメチオニンのみにした

--- js/kero_browse_track_custom.js
+++ js/kero_browse_track_custom.js
@@ -728,18 +728,18 @@ WgFastaAmino.prototype.paint = function(y, width, chr, start, end, strand) {
 					var x4 = x2 + (width - 1) / (end - start + 1);
 					var genetic_code = {
 						UUU: "Phe", UUC: "Phe", UUA: "Leu", UUG: "Leu",
-						UCU: "Ser", UCC: "Ser", UCA: "Ser", UCG: "Ser>",
+						UCU: "Ser", UCC: "Ser", UCA: "Ser", UCG: "Ser",
 						UAU: "Tyr", UAC: "Tyr", UAA: "Stop", UAG: "Stop",
 						UGU: "Cys", UGC: "Cys", UGA: "Stop", UGG: "Trp",
 						CUU: "Leu", CUC: "Leu", CUA: "Leu", CUG: "Leu",
 						CCU: "Pro", CCC: "Pro", CCA: "Pro", CCG: "Pro",
 						CAU: "His", CAC: "His", CAA: "Gln", CAG: "Gln",
 						CGU: "Arg", CGC: "Arg", CGA: "Arg", CGG: "Arg",
-						AUU: "Ile", AUC: "Ile", AUA: "Ile", AUG: "Met>",
+						AUU: "Ile", AUC: "Ile", AUA: "Ile", AUG: "Met",
 						ACU: "Thr", ACC: "Thr", ACA: "Thr", ACG: "Thr",
 						AAU: "Asn", AAC: "Asn", AAA: "Lys", AAG: "Lys",
 						AGU: "Ser", AGC: "Ser", AGA: "Arg", AGG: "Arg",
-						GUU: "Val", GUC: "Val", GUA: "Val", GUG: "Val>",
+						GUU: "Val", GUC: "Val", GUA: "Val", GUG: "Val",
 						GCU: "Ala", GCC: "Ala", GCA: "Ala", GCG: "Ala",
 						GAU: "Asp", GAC: "Asp", GAA: "Glu", GAG: "Glu",
 						GGU: "Gly", GGC: "Gly", GGA: "Gly", GGG: "Gly",
@@ -766,9 +766,6 @@ WgFastaAmino.prototype.paint = function(y, width, chr, start, end, strand) {
 						"Tyr": "#77D9A9",
 						"Val": "#FFF100",
 						"Stop": "#000000",
-						"Ser>": "#BFE4FF",
-						"Met>": "#BFE4FF",
-						"Val>": "#BFE4FF",
 					};
 					var amino_code = {
 						"Ala": "A",
@@ -792,9 +789,6 @@ WgFastaAmino.prototype.paint = function(y, width, chr, start, end, strand) {
 						"Tyr": "Y",
 						"Val": "V",
 						"Stop": "",
-						"Ser>": ">",
-						"Met>": ">",
-						"Val>": ">",
 					};
 					var codon = "";
 					codon += char0.toUpperCase() == "T" ? "U" : char0.toUpperCase();
